4387204caed0269055b3fd234fb204175d10f9fe
Alex Beregszaszi <alex@rtfs.hu>
2017-02-06 20:21:27 +0000

    Add REVERT to libevmasm

--- libevmasm/PeepholeOptimiser.cpp
+++ libevmasm/PeepholeOptimiser.cpp
@@ -201,6 +201,7 @@ struct UnreachableCode
 			it[0] != Instruction::STOP &&
 			it[0] != Instruction::INVALID &&
 			it[0] != Instruction::SELFDESTRUCT
+			it[0] != Instruction::REVERT
 		)
 			return false;
 
